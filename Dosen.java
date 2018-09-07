4bfdaa1dddb124897e890e209e291d3b6c8a2051
Helvetica Standard <abdillah.syafiq@gmail.com>
2018-09-07 19:24:53 +0700

    dosen.java to branch temporaryBranch

---
+++ Dosen.java
@@ -0,0 +1,15 @@
+public class Dosen {
+    String nama;
+
+    Dosen(String namaa){
+        nama = nama;
+    }
+
+    public void setNama( String namabaru){
+
+    }
+
+    public String getNama(){
+        return nama;
+    }
+}
